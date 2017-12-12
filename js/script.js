f89b8761c448258a33829bf79d91aa4512636787
Nickel 13 <nikesushko@gmail.com>
2017-12-12 15:57:07 +0300

    22nd
    Поправлен .js для кнопки

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
-var open = document.querySelector(".search-window__js-button");
-var popup = document.querySelector(".search-form");
-var close = popup.querySelector(".search-window__js-button");
-var focus = popup.querySelector("[name=name]");
+  var searchButton = document.querySelector(".search-window__js-button");
+  var openForm = document.querySelector(".search-form");
 
-open.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.add("search-form--show");
-  focus.focus();
-});
+  searchButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openForm.classList.toggle("search-form--show");})
 
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.remove("search-form--show");
-});
+// var open = document.querySelector(".search-window__js-button");
+// var popup = document.querySelector(".search-form");
+// var close = popup.querySelector(".search-window__js-button");
+// var focus = popup.querySelector("[name=name]");
 
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("search-form--show")) {
-      popup.classList.remove("search-form--show");
-    }
-  }
-});
+// open.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   popup.classList.add("search-form--show");
+//   focus.focus();
+// });
+
+// close.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   popup.classList.remove("search-form--show");
+// });
+
+// window.addEventListener("keydown", function(event) {
+//   if (event.keyCode === 27) {
+//     if (popup.classList.contains("search-form--show")) {
+//       popup.classList.remove("search-form--show");
+//     }
+//   }
+// });
