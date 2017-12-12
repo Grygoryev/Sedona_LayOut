7438b138b12876d58f9ff644562455a6aad009af
Nickel 13 <nikesushko@gmail.com>
2017-12-12 14:07:33 +0300

    20th commit
    Исправлен footer
    Добавлен скрипт к кнопке, правда пока не работающий

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var open = document.querySelector(".search-window__js-button");
+var popup = document.querySelector(".search-form");
+var close = popup.querySelector(".search-window__js-button");
+var focus = popup.querySelector("[name=name]");
+
+open.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("search-form--show");
+  focus.focus();
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("search-form--show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("search-form--show")) {
+      popup.classList.remove("search-form--show");
+    }
+  }
+});
